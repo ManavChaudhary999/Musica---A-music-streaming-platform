d1da7052caeb9b6bc8556acddb19eb21f5c5c81b
ManavChaudhary999 <999macboy@gmail.com>
2023-03-31 17:52:30 +0530

    Updated Sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './Sidebar.module.css';
 
 import Logo from "../../assets/logo";
-import {Home, Disc, BarChart2, MapPin, Heart, PlusSquare, User, AlignJustify} from "react-feather";
+import {Home, BarChart2, MapPin, Heart, PlusSquare, User, AlignJustify} from "react-feather";
 
 function Sidebar() {
   return (
@@ -10,19 +10,37 @@ function Sidebar() {
       <img src={Logo} alt="Logo" className={styles.logo} />
       {/* App Nav */}
       <nav className={styles.nav}>
-          <button>
-            <Home />
-            <span>Discover</span>
+          <button className={styles.button}>
+            <div className={styles.icon}><Home /></div>
+            <span className={styles.span}>Discover</span>
           </button>
-          <button><MapPin /></button>
-          <button><BarChart2 /></button>
+          <button className={styles.button}>
+            <div className={styles.icon}><MapPin /></div>
+            <span className={styles.span}>Around You</span>
+          </button>
+          <button className={styles.button}>
+            <div className={styles.icon}><BarChart2 /></div>
+            <span className={styles.span}>Top Charts</span>
+          </button>
       </nav>
       {/* Personal Nav */}
       <nav className={styles.nav}>
-          <button><PlusSquare /></button>
-          <button><Heart /></button>
-          <button><AlignJustify /></button>
-          <button><User /></button>
+        <button className={styles.button}>
+          <div className={styles.icon}><PlusSquare /></div>
+          <span className={styles.span}>Create Playlist</span>
+        </button>
+        <button className={styles.button}>
+          <div className={styles.icon}><Heart /></div>
+          <span className={styles.span}>Liked Songs</span>
+        </button>
+        <button className={styles.button}>
+          <div className={styles.icon}><AlignJustify /></div>
+          <span className={styles.span}>Mac's Playlist</span>
+        </button>
+        <button className={styles.button}>
+          <div className={styles.icon}><User /></div>
+          <span className={styles.span}>Manage</span>
+        </button>
       </nav>
     </div>
   );
